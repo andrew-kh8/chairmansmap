c46b452decc08641098cdbc2a086c0f675dd2ff3
andrew-kh8 <horolskyandrey@gmail.com>
2026-01-11 00:57:23 +0300

    change daterangepicker to plain date_field

--- app/controllers/side_panel/hunters_controller.rb
+++ app/controllers/side_panel/hunters_controller.rb
@@ -1,8 +1,13 @@
 module SidePanel
   class HuntersController < ApplicationController
     def index
-      hunter_locations = HunterLocation.all
-      render partial: "index", locals: {hunter_locations: hunter_locations}
+      hunter_locations = HunterLocationSearch.call(search_params)
+      locals = {
+        hunter_locations: hunter_locations,
+        dog_options: HunterLocationSearch::DOG_OPTIONS,
+        license_options: HunterLocationSearch::LICENSE_OPTIONS
+      }
+      render partial: "index", locals:
     end
 
     def new
@@ -20,6 +25,17 @@ module SidePanel
 
     def hunter_params
       params.require(:hunter_location).permit(:date, :license, :dog, :description, :location)
+    end
+
+    def search_params
+      hunter_location = params.fetch(:hunter_location, {})
+
+      {
+        from: hunter_location[:from],
+        to: hunter_location[:to],
+        dog: hunter_location[:dog],
+        license: hunter_location[:license]
+      }.compact_blank
     end
   end
 end
---
+++ app/queries/hunter_location_search.rb
@@ -0,0 +1,32 @@
+class HunterLocationSearch
+  DOG_OPTIONS = {"есть" => true, "нет" => false}
+  LICENSE_OPTIONS = {"есть" => true, "нет" => false}
+
+  module Scopes
+    def by_date(date_from, date_to)
+      return self if date_from.blank? && date_to.blank?
+
+      where(date: date_from..date_to)
+    end
+
+    def by_dog(dog)
+      return self if dog.nil?
+
+      where(dog:)
+    end
+
+    def by_license(license)
+      return self if license.nil?
+
+      where(license:)
+    end
+  end
+
+  def self.call(filters = {})
+    HunterLocation
+      .extending(Scopes)
+      .by_date(filters[:from], filters[:to])
+      .by_dog(filters[:dog])
+      .by_license(filters[:license])
+  end
+end
