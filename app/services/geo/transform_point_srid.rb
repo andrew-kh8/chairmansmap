f6f0b4230f82ba160fdfc6a0cf5d621f4ba2380b
andrew-kh8 <horolskyandrey@gmail.com>
2026-01-13 00:03:45 +0300

    upd tests for ci

--- app/services/geo/transform_point_srid.rb
+++ app/services/geo/transform_point_srid.rb
@@ -5,7 +5,7 @@ module Geo
 
     def self.call(lng, lat, from_srid = 4326)
       point_srid = "SRID=#{from_srid};POINT(#{lng} #{lat})"
-      sql_query = "SELECT ST_AsText(ST_Transform(ST_GeomFromText(?), ?)) AS #{TRANSFORMED_ATTR}"
+      sql_query = "SELECT ST_AsText(ST_Transform(ST_GeomFromEWKT(?), ?)) AS #{TRANSFORMED_ATTR}"
       sanitized_query = ActiveRecord::Base.sanitize_sql([sql_query, point_srid, SRID])
 
       ActiveRecord::Base.connection.execute(sanitized_query).first[TRANSFORMED_ATTR]
