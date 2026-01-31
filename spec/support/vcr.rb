54c107416651ee69251db2871d8174228356f63b
andrew-kh8 <horolskyandrey@gmail.com>
2026-01-31 22:01:50 +0300

    config vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,4 +2,6 @@ VCR.configure do |config|
   config.cassette_library_dir = Rails.root.join("spec/fixtures/vcr_cassettes")
   config.allow_http_connections_when_no_cassette = true
   config.hook_into :faraday
+  config.default_cassette_options = {record: ENV["COVERAGE"] ? :none : :once}
+  config.ignore_localhost = true
 end
