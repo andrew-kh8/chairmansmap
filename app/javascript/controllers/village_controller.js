11000236b42a3bc4c728ce19381a8a0b8e938b7e
andrew-kh8 <horolskyandrey@gmail.com>
2026-03-07 23:21:51 +0300

    agro tiles for village

--- app/javascript/controllers/village_controller.js
+++ app/javascript/controllers/village_controller.js
@@ -7,14 +7,41 @@ export default class extends Controller {
     id: String,
   };
 
-  static targets = ["map"];
+  static targets = ["map", "tile"];
+  btnClasses = [
+    "text-white",
+    "dark:text-white",
+    "bg-btn-green",
+    "dark:bg-btn-green",
+  ];
 
   connect() {
     this.leafletMap = new LeafletMap(this.mapTarget);
     this.map = this.leafletMap.initMap();
 
+    this.extraTileBtn = null;
+    this.extraTileLayer = null;
     this.#showVillage();
   }
+
+  showTile(event) {
+    this.#removeExtraTileLayer();
+
+    if (this.extraTileBtn) {
+      this.extraTileBtn.classList.remove(...this.btnClasses);
+    }
+
+    if (this.extraTileBtn === event.currentTarget) {
+      this.extraTileBtn = null;
+      return;
+    }
+
+    this.extraTileBtn = event.currentTarget;
+    this.#addExtraTileLayer();
+    this.extraTileBtn.classList.add(...this.btnClasses);
+  }
+
+  // private
 
   #showVillage() {
     fetch(`/geometry/villages/${this.idValue}`)
@@ -28,5 +55,17 @@ export default class extends Controller {
 
         this.map.fitBounds(this.wfs_village_layer.getBounds());
       });
+  }
+
+  #addExtraTileLayer() {
+    this.extraTileLayer = L.tileLayer(this.extraTileBtn.dataset.tileValue);
+    this.extraTileLayer.addTo(this.map);
+  }
+
+  #removeExtraTileLayer() {
+    if (this.extraTileLayer) {
+      this.map.removeLayer(this.extraTileLayer);
+      this.extraTileLayer = null;
+    }
   }
 }
